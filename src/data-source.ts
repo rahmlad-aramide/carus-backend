bc34a3c6f65158c70ae100995946b027180b337c
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2025-11-14 12:31:53 +0100

    fix: merge conflict

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,21 +11,9 @@ export const AppDataSource = new DataSource({
   username: env.DB_USERNAME,
   password: String(process.env.DB_PASSWORD),
   database: process.env.DB_NAME,
-  // entities: [
-  //   './dist/entities/*.js'
-  // ],
-  // migrations: [
-  //   './dist/migrations/*.js' // Same for migrations
-  // ],
-  entities: [path.join(__dirname + '/entities/*.{js,ts}')],
-  migrations: [path.join(__dirname + '/migrations/*.{js,ts}')],
-  // entities: ['**/entities/*.ts'],
-  // migrations: ['**/migrations/*.ts'],
-  // entities: ['./entities/*.ts', './entities/*.js'],
-  // migrations: ['./migrations/*.ts', './migrations/*.js'],
-  // entities: [__dirname + '/entities/*.{js,ts}'],
-  // migrations: [__dirname + '/migrations/*.{js,ts}'],
-  synchronize: true,
+  entities: [path.join(__dirname, "/entities/*.{js,ts}")],
+  migrations: [path.join(__dirname, "/migrations/*.{js,ts}")],
+  synchronize: process.env.NODE_ENV === 'production' ? false : true,
   poolSize: 10,
   ssl: {
     rejectUnauthorized: false,
